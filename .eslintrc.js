e588bdb561aa9eb647d64f0e117dfdfe8cae1b98
Chris-matrix <cwatt0046@launchapd.org>
2025-03-27 23:28:21 -0400

    Tron-timer2

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'import',
+  ],
+  rules: {
+    // React specific rules
+    'react/prop-types': 'warn',
+    'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    
+    // General JavaScript rules
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'no-undef': 'error',
+    'prefer-const': 'warn',
+    
+    // Import rules
+    'import/no-unresolved': 'error',
+    'import/named': 'error',
+    'import/default': 'error',
+    'import/namespace': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+};
